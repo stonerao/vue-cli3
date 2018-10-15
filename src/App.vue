c4dc4a9e546e724c60a203dcb923366310992715
stone <674656681@qq.com>
2018-10-15 18:29:16 +0800

    迭代
    Signed-off-by: stone <674656681@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div id="app">  
+  <div id="app">
     <router-view/>
   </div>
 </template>
+<script>
+export default {};
+</script>
 
-<style lang="less"> 
+<style lang="less">
+@import url("./styles/reset.less");
 </style>
